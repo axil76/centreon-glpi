a42f782a3b0fc36723a86980c2ab0694c10619df
Sylvestre Ho <sho@merethis.com>
2012-06-13 16:58:12 +0200

    restart poller if ip address is updated

--- conf.php
+++ conf.php
@@ -1,11 +1,11 @@
 <?php
-$module_conf['centreon-glpi']["rname"] = "Centreon GLPI";
-$module_conf['centreon-glpi']["name"] = "centreon-glpi";
-$module_conf['centreon-glpi']["mod_release"] = "1.0.0";
-$module_conf['centreon-glpi']["infos"] = "GLPI Module";
-$module_conf['centreon-glpi']["is_removeable"] = "1";
-$module_conf['centreon-glpi']["author"] = "Merethis Team";
-$module_conf['centreon-glpi']["lang_files"] = "0";
-$module_conf['centreon-glpi']["sql_files"] = "1";
-$module_conf['centreon-glpi']["php_files"] = "0";
+$module_conf['centreon\-glpi']["rname"] = "Centreon GLPI";
+$module_conf['centreon\-glpi']["name"] = "centreon\-glpi";
+$module_conf['centreon\-glpi']["mod_release"] = "1.0.0";
+$module_conf['centreon\-glpi']["infos"] = "GLPI Module";
+$module_conf['centreon\-glpi']["is_removeable"] = "1";
+$module_conf['centreon\-glpi']["author"] = "Merethis Team";
+$module_conf['centreon\-glpi']["lang_files"] = "0";
+$module_conf['centreon\-glpi']["sql_files"] = "1";
+$module_conf['centreon\-glpi']["php_files"] = "0";
 ?>
