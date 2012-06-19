29d32a402276f1f7baf05772ba36423b68e07412
Sylvestre Ho <sho@merethis.com>
2012-06-19 10:00:57 +0200

    prepare 1.0.2

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
 $module_conf['centreon-glpi']["rname"] = "Centreon GLPI";
 $module_conf['centreon-glpi']["name"] = "centreon-glpi";
-$module_conf['centreon-glpi']["mod_release"] = "1.0.1";
+$module_conf['centreon-glpi']["mod_release"] = "1.0.2";
 $module_conf['centreon-glpi']["infos"] = "GLPI Module";
 $module_conf['centreon-glpi']["is_removeable"] = "1";
 $module_conf['centreon-glpi']["author"] = "Merethis Team";
